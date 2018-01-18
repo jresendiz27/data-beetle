eb93adb09de1c0fbe7063a27a3ce22bb9f780e59
egjimenezg <egjimenezg@gmail.com>
2018-01-18 00:32:35 -0600

    #1 updating HtmlDocumentInformation class name

---
+++ scrapping/src/main/groovy/mx/databeetle/scrapping/HtmlDocument.groovy
@@ -0,0 +1,69 @@
+package mx.databeetle.scrapping
+
+import groovy.transform.CompileStatic
+import groovy.util.logging.Log4j2
+import mx.databeetle.core.api.HtmlInformation
+import mx.databeetle.core.sanitizer.StringSanitizer
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+import org.jsoup.safety.Cleaner
+import org.jsoup.safety.Whitelist
+
+@Log4j2
+@CompileStatic
+class HtmlDocument implements HtmlInformation {
+
+  Document document
+  String content
+
+  HtmlDocument(String content){
+    this.content = content
+    this.document = new Cleaner(Whitelist.basic()).clean(Jsoup.parse(this.content))
+  }
+
+  @Override
+  Map<String, Long> getWordsCount() {
+    Map<String, Long> words = [:]
+      String bodyText = StringSanitizer.sanitizeContent(document.body().text())
+      bodyText.split(" ").each { String word ->
+        if (words.get(word) != null) {
+          Long wordQuantity = words.get(word) + 1
+            words.put(word, wordQuantity)
+        } else {
+          words.put(word, (Long) 1)
+        }
+      }
+    return words
+  }
+
+  @Override
+  Map<String, String> getMetaTagsInformation() {
+    return null
+  }
+
+  @Override
+  void removeUnusedDOMElements() {
+    ["script","style","link","title"].each{ tag ->
+      removeElements(document.getElementsByTag(tag))
+    }
+  }
+
+  @Override
+  ArrayList<String> getDocumentLinks(){
+    ArrayList<String> links = []
+    document.select("a")*.attr("href")
+                         .findAll{ link -> link.startsWith("http") }
+                         .each{ link -> links << link }
+    links
+  }
+
+  static void removeElements(Elements elements) {
+    elements.each {
+      Element tagElement = (Element) it
+      tagElement.remove()
+    }
+  }
+
+}
